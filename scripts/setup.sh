608a9cce6c3958ffdbb328f3a15b465fa6ff8fd5
Dennis Durairaj <dennis.durairaj@gmail.com>
2024-06-28 20:02:32 +0200

    Add more packages to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,18 +10,29 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	chsh -s $(which zsh)
 	# zoxide
 	curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+	# ripgrep
+	apt-get install ripgrep
+	# fd
+	apt-get install fd-find
 	# WezTerm
-	curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
-	echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+	curl -fsSL https://apt.fury.io/wez/gpg.key | gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+	echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | tee /etc/apt/sources.list.d/wezterm.list
 	apt-get update
 	apt-get install wezterm
+	# lazygit
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf lazygit.tar.gz lazygit
+	install lazygit /usr/local/bin
 
 # Mac
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	brew install git
+	brew install jesseduffield/lazygit/lazygit
 	brew install fzf
 	brew install zoxide
+	brew install ripgrep
 	brew install --cask wezterm
 fi
 
